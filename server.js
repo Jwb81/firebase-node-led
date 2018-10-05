9566aadd1c815da12df58d89bb17b593f9b454ef
Jason Barnett <jayb21@neo.rr.com>
2018-10-05 00:36:41 -0400

    added some console logs for testing the server

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ var com = new SerialPort('/dev/ttyACM0', {
 })
 
 com.on('data', function(data) {
-	console.log('Data: ', data.toString());
+	console.log(data.toString());
 })
 
 com.on('error', function(err) {
@@ -102,12 +102,14 @@ function remoteLED(rgb, active, id) {
     let values = '<' + id + ',' + rgb.red + ',' + rgb.green + ',' + rgb.blue + ',' + onOff + '>';
 
     // send the string over serial
+    console.log('before com.write()');
     com.write(values, function(err) {
-		if (err)
-			return console.log('Error on write: ', err.message);
-
+        if (err)
+        return console.log('Error on write: ', err.message);
+        
 		// console.log('message written');
 	})	
+    console.log('after com.write()');
 }
 
 io.sockets.on('connection', function (socket) { // Web Socket Connection
