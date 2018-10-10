235b1cbf525c6c9174d18936090edd84afcb059b
Jason Barnett <jayb21@neo.rr.com>
2018-10-10 00:22:58 -0400

    changing light functionality in the database

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -37,37 +37,6 @@ var database = firebase.database();
 
 
 // FUNCTIONS
-// var resetDb = function (values) {
-    
-    
-//     // var init = {
-//     //     red: 0,
-//     //     green: 0,
-//     //     blue: 0,
-//     //     active: false,
-//     //     'lighting-groups': [
-//     //         {
-//     //             room: `Baby's room 16ft`,
-//     //             rgb: {
-//     //                 red: 0,
-//     //                 green: 0,
-//     //                 blue: 0
-//     //             }
-//     //         },
-//     //         {
-//     //             room: `Baby's room 5ft`,
-//     //             rgb: {
-//     //                 red: 0,
-//     //                 green: 0,
-//     //                 blue: 0
-//     //             }
-//     //         }
-//     //     ]
-//     // }
-
-//     // database.ref().set(init)
-// }
-
 var setData = function () {
     // console.log(rgb.red);
     // console.log(password);
@@ -84,33 +53,33 @@ var setData = function () {
 
 // connectionsRef references a specific location in our database.
 // All of our connections will be stored in this directory.
-var connectionsRef = database.ref("/connections");
+// var connectionsRef = database.ref("/connections");
 
 // '.info/connected' is a special location provided by Firebase that is updated
 // every time the client's connection state changes.
 // '.info/connected' is a boolean value, true if the client is connected and false if they are not.
-var connectedRef = database.ref(".info/connected");
+// var connectedRef = database.ref(".info/connected");
 
 // When the client's connection state changes...
-connectedRef.on("value", function (snap) {
+// connectedRef.on("value", function (snap) {
 
-    // If they are connected..
-    if (snap.val()) {
+//     // If they are connected..
+//     if (snap.val()) {
 
-        // Add user to the connections list.
-        var con = connectionsRef.push(true);
-        // Remove user from the connection list when they disconnect.
-        con.onDisconnect().remove();
-    }
-});
+//         // Add user to the connections list.
+//         var con = connectionsRef.push(true);
+//         // Remove user from the connection list when they disconnect.
+//         con.onDisconnect().remove();
+//     }
+// });
 
 // When first loaded or when the connections list changes...
-connectionsRef.on("value", function (snap) {
+// connectionsRef.on("value", function (snap) {
 
     // Display the viewer count in the html.
     // The number of online users is the number of children in the connections list.
     // $("#connected-viewers").text(snap.numChildren());
-});
+// });
 
 // ------------------------------------
 // Initial Values
@@ -118,50 +87,47 @@ connectionsRef.on("value", function (snap) {
 // --------------------------------------------------------------
 // At the page load and subsequent value changes, get a snapshot of the local data.
 // This function allows you to update your page in real-time when the values within the firebase node bidderData changes
-database.ref().on("value", function (snapshot) {
-    var info = snapshot.val();
+database.ref('/lights').on("value", function (snapshot) {
+    var lights = snapshot.val();
 
     // If Firebase has a highPrice and highBidder stored (first case)
-    if (snapshot.child("red").exists() &&
-        snapshot.child("green").exists() &&
-        snapshot.child("blue").exists() &&
-        snapshot.child("active").exists() 
-        // && snapshot.child("password").exists()
-        ) {
+    // if (snapshot.child("red").exists() &&
+    //     snapshot.child("green").exists() &&
+    //     snapshot.child("blue").exists() &&
+    //     snapshot.child("active").exists() 
+    //     // && snapshot.child("password").exists()
+    //     ) {
 
-        // password = info.password;
+    //     // password = info.password;
 
-        rgb.red = info.red; //Update the RED color according to the picker
-        rgb.green = info.green; //Update the GREEN color according to the picker
-        rgb.blue = info.blue; //Update the BLUE color according to the picker
+    //     rgb.red = info.red; //Update the RED color according to the picker
+    //     rgb.green = info.green; //Update the GREEN color according to the picker
+    //     rgb.blue = info.blue; //Update the BLUE color according to the picker
 
-        colorShow.style.backgroundColor = rgb.toRgbString(); //update the "Current color"
+    //     colorShow.style.backgroundColor = rgb.toRgbString(); //update the "Current color"
 
-        rSlider.value = rgb.red; //Update the RED slider position according to the picker
-        gSlider.value = rgb.green; //Update the GREEN slider position according to the picker
-        bSlider.value = rgb.blue; //Update the BLUE slider position according to the picker
+    //     rSlider.value = rgb.red; //Update the RED slider position according to the picker
+    //     gSlider.value = rgb.green; //Update the GREEN slider position according to the picker
+    //     bSlider.value = rgb.blue; //Update the BLUE slider position according to the picker
 
-        redNum.value = rgb.red;
-        greenNum.value = rgb.green;
-        blueNum.value = rgb.blue;
+    //     redNum.value = rgb.red;
+    //     greenNum.value = rgb.green;
+    //     blueNum.value = rgb.blue;
 
-        active = info.active;
-        document.getElementById('active').checked = active;
-    }
+    //     active = info.active;
+    //     document.getElementById('active').checked = active;
+    // }
 
-    // Else Firebase doesn't have a highPrice/highBidder, so use the initial local values.
-    else {
-        console.log('Database needs created filled or created...');
 
-    }
+
 
     // If any errors are experienced, log them to console.
 }, function (errorObject) {
     console.log("The read failed: " + errorObject.code);
 });
 
-// do something with new lighting groups and 
-database.ref('/lights').on('value', function (snap) {
+// display each light to the user with a checkbox next to it
+database.ref('/lights').once('value', function (snap) {
     var lights = snap.val();
 
     // empty the section before refilling it with new values
@@ -206,8 +172,8 @@ $('#select-all-lights').on('click', function() {
         lightGroup = lightGroup.concat($(boxes[i]).data('light-id'));
     }
 
+    // try a forEach loop instead of a for loop above
 
-    // console.log(lightGroup);
 })
 
 $('#select-none-lights').on('click', function() {
@@ -215,8 +181,6 @@ $('#select-none-lights').on('click', function() {
     
     // clear the lightGroup array
     lightGroup = [];
-
-    // console.log(lightGroup);
 })
 
 // add a lighting group to the active array when it is checked
@@ -227,7 +191,7 @@ $('#lighting-groups').on('change', '.lighting-group-checkbox', function() {
     if (index == -1)
         lightGroup = lightGroup.concat(val);
     else
-        lightGroup.splice(index, 1);
+        lightGroup = lightGroup.slice(index, index+1);
 
     // console.log(lightGroup);
 })
